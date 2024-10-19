381d2757aeb1bbaff0ad137eb83098e251480f70
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2024-10-19 11:17:40 -0500

    Creacion de la estructura del proyecto

--- src/components/Pages/Home/Home.jsx
+++
@@ -1,9 +0,0 @@
-import {Header} from '../../Layouts/Header/Header.jsx'
-
-export const Home = () => {
-  return (
-    <>
-      <div>Home</div>
-    </>
-  )
-}
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { App } from './App.jsx'
+import { App } from './components/Pages/App/App'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
